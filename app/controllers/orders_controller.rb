590952eef62dd0e933b14fd0469da2042ec6df5b
ayumi-tanaka <67615448+ayumi-tanaka@users.noreply.github.com>
2020-10-06 11:49:17 +0900

    indexアクションの定義

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,11 @@
 class OrdersController < ApplicationController
 
   def index
+    return redirect_to root_path if current_user.id == select_item.user_id || select_item.order!=nil
+
   end
 
   def create
   end
-  
+
 end
