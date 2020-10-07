aa42b5499ede85809868bff8eb7b70c7aa8b42c7
ayumi-tanaka <67615448+ayumi-tanaka@users.noreply.github.com>
2020-10-07 23:33:50 +0900

    pay_formの設定
    orders_controllerの修正

--- app/forms/pay_form.rb
+++ app/forms/pay_form.rb
@@ -1,4 +1,4 @@
 class PayForm
   include ActiveModel::Model
- attr_accessor :item_id, :token, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :user_id
+  attr_accessor :item_id, :token, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :user_id
 end
