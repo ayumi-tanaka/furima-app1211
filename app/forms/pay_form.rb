2077ed4f7470bce2fa85499fedae39b221192531
ayumi-tanaka <67615448+ayumi-tanaka@users.noreply.github.com>
2020-10-11 09:28:31 +0900

    フォームオブジェクトの編集

--- app/forms/pay_form.rb
+++ app/forms/pay_form.rb
@@ -1,4 +1,19 @@
 class PayForm
   include ActiveModel::Model
   attr_accessor :item_id, :token, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :user_id
+  def save
+    order = Order.create(
+      item_id: item_id,
+      user_id: user_id
+    )
+    Address.create(
+      order_id: order.id,
+      postal_code: postal_code,
+      prefecture_id: prefecture_id,
+      city: city,
+      addresses: addresses,
+      building: building,
+      phone_number: phone_number
+    )
+  end
 end
