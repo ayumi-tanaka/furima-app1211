b51f8be3b1c35852cd0d0b316400587691455de6
ayumi-tanaka <67615448+ayumi-tanaka@users.noreply.github.com>
2020-10-11 09:45:09 +0900

    pay_formの編集

--- app/forms/pay_form.rb
+++ app/forms/pay_form.rb
@@ -1,6 +1,18 @@
 class PayForm
   include ActiveModel::Model
   attr_accessor :item_id, :token, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :user_id
+  
+  with_options presence: true do
+    validates :item_id
+    validates :token, presence: { message: "can't be blank" }
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'Input correctly' }
+    validates :prefecture_id, numericality: { other_than: 0, message: 'Select' }
+    validates :city
+    validates :addresses
+    validates :phone_number, length: { maximum: 11, message: 'Too long' }
+    validates :user_id
+  end
+
   def save
     order = Order.create(
       item_id: item_id,
